879c3637a8cbd47cbc8987068feac1cd4b480091
ASHOK MAHARJAN <ashok997@gmail.com>
2019-05-12 16:15:23 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -20,3 +20,11 @@ def take_a_number (array,name)
     index =array.index(name) + 1
     puts "Welcome, #{name}. You are number #{index} in line."
 end
+
+def now_serving(array)
+  if array.size==0
+    puts "The line is empty"
+  else
+    puts "Currently serving #{array[0]}."
+  end
+end
