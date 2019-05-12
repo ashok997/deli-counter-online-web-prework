3310fafe34fdd7c50ddb1bebb2e36992bdf4890b
ASHOK MAHARJAN <ashok997@gmail.com>
2019-05-12 16:07:59 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,22 @@
-# Write your code here.
+# Write your code here
+katz_deli = []
+
+def line (array)
+  if array.size==0
+    puts "The line is currently empty."
+  else
+    counter = 0
+    print "The line is currently :"
+    array.each do |name|
+      puts counter
+      puts name
+      counter+=1
+    end
+  end
+end
+
+def take_a_number (array,name)
+    array.push(name)
+    index =array.index(name) + 1
+    puts "Welcome, #{name}. Your are number #{index} in line."
+end
