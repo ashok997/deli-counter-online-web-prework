962c9dd60f8cf31d9e5a1554a8fe35ffb8ffca6f
ASHOK MAHARJAN <ashok997@gmail.com>
2019-05-12 16:21:32 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -9,7 +9,7 @@ def line (array)
     message = "The line is currently :"
     array.each do |name|
       counter+=1
-      message << "#{counter}" << "message"
+      message << "#{counter}" << "#{message}"
     end
     puts message
   end
